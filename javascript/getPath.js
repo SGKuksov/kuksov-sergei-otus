c87dca0bafa91c78dcc3e0b2e1df0a1f8504ff4b
Sergei Kuksov <sgkuksov1989@gmail.com>
2019-10-12 00:49:08 +0300

    Add recursion

--- javascript/getPath.js
+++ javascript/getPath.js
@@ -10,18 +10,35 @@ const paragraph2 = [...document.querySelectorAll('p')][0];
 const getPath = element => {
   let result = null;
 
-  if (element.tagName === 'html') return 'html';
-  if (element.tagName === 'body') return 'html > body';
-  const b = document.querySelector('html > body');
+  const getElemName = elem => {
+    return elem.nodeName.toLocaleLowerCase()
+  };
+
+  const getParent = (elem, acc) => {
+    if (getElemName(elem) !== 'html' ) {
+      return getParent(elem.parentElement, `${getElemName(elem.parentElement)} > ${acc}`)
+    } else {
+      return acc
+    }
+  };
+
+  if (getElemName(element) === 'html') return 'html';
+  if (
+    getElemName(element) === 'body' && 
+    getElemName(element.parentElement) === 'html'
+  ) return 'html > body';
+
+  result = getParent(element, getElemName(element))
+  console.log(result)
+  return result
 
   // tagName
+  // parentElement
   // body > p:nth-child(4) > b
-
-  return result;
 };
 
 const element1 = document.querySelector(getPath(paragraph1));
 const element2 = document.querySelectorAll(getPath(paragraph2));
 
-console.assert([...element2].length === 1);
+// console.assert([...element2].length === 1);
 console.assert(element1 === [...element2][0]); // Сравнение по ссылке .D
